33c595560250859fb73b9d771a4a20a9b7bf468f
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-10-22 11:24:11 +0530

    reviews and review detai graphql

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,22 +1,40 @@
 import React from "react";
 import useFetch from "../hooks/useFetch";
+import { useQuery, gql } from "@apollo/client";
 import { useParams } from "react-router-dom";
+
+const REVIEW = gql`
+  query GetReview($id: ID!) {
+    review(id: $id) {
+      data {
+        attributes {
+          title
+          rating
+          body
+        }
+      }
+    }
+  }
+`;
 
 export default function ReviewDetails({}) {
   const { id } = useParams();
-  const { data, loading, error } = useFetch(
-    `http://localhost:1337/api/reviews/${id}`
-  );
+  // const { data, loading, error } = useFetch(
+  //   `http://localhost:1337/api/reviews/${id}`
+  // );
+  const { data, loading, error } = useQuery(REVIEW, {
+    variables: { id },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
   return (
     <div className="review-details">
       <div className="review-card">
-        <div className="rating"> {data.attributes.rating}</div>
-        <h2>{data.attributes.title}</h2>
+        <div className="rating"> {data.review.data.attributes.rating}</div>
+        <h2>{data.review.data.attributes.title}</h2>
         <small>console list</small>
-        <p>{data.attributes.body}</p>
+        <p>{data.review.data.attributes.body}</p>
       </div>
     </div>
   );
